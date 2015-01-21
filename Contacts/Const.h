74beeff2014de8ebd31e9edaf4710da611881eda
Spencer Morris <spencer@ticketscalpr.com>
2015-01-21 16:49:49 -0600

    Setup constants class

---
+++ Contacts/Const.h
@@ -0,0 +1,15 @@
+//
+//  Const.h
+//  Contacts
+//
+//  Created by Spencer Morris on 1/21/15.
+//  Copyright (c) 2015 SolsticeMobile. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#define API_BASE_URL [NSURL URLWithString:@"https://solstice.applauncher.com/external/"]
+
+@interface Const : NSObject
+
+@end
