8791c8faf9ae67694454fe4fa6af3d942fa687f4
Spencer Morris <spencer@ticketscalpr.com>
2015-01-21 16:35:15 -0600

    Create API client

---
+++ Contacts/ApiClient.h
@@ -0,0 +1,13 @@
+//
+//  ApiClient.h
+//  Contacts
+//
+//  Created by Spencer Morris on 1/21/15.
+//  Copyright (c) 2015 SolsticeMobile. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ApiClient : NSObject
+
+@end
---
+++ Contacts/ApiClient.m
@@ -0,0 +1,13 @@
+//
+//  ApiClient.m
+//  Contacts
+//
+//  Created by Spencer Morris on 1/21/15.
+//  Copyright (c) 2015 SolsticeMobile. All rights reserved.
+//
+
+#import "ApiClient.h"
+
+@implementation ApiClient
+
+@end
